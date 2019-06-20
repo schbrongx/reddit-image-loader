f2cb31df7103497706b375e62fd2a39a5366779e
Thomas Pesendorfer <pesendorfer@was.ch>
2019-06-20 12:55:30 +0200

    bugfix: removed trailing slash from path

--- src/cronscript.sh
+++ src/cronscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR=/home/t.pesendorfer/Repositories/reddit-image-loader.git/
-$DIR/venv/bin/python $DIR/src/ril.py -T -l 25 earthporn /home/t.pesendorfer/Bilder/desktop_backgrounds
+$DIR/venv/bin/python $DIR/src/ril.py -H -l 25 earthporn /home/t.pesendorfer/Bilder/desktop_backgrounds
